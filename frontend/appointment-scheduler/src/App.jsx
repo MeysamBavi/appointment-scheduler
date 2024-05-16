d7fb15474d3795bc53abe1ff63ef5d0a3c22fd05
Abbas Yazdanmehr <kjkj55047@gmail.com>
2024-05-16 09:46:37 +0330

    feat(frontend): add business page

--- frontend/appointment-scheduler/src/App.jsx
+++ frontend/appointment-scheduler/src/App.jsx
@@ -1,15 +1,15 @@
-import './styles/App.css'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import NotFound from './pages/NotFound';
-import Login from './pages/Login';
-import Landing from './pages/Landing';
-import OwnerForm from './pages/OwnerForm';
+import "./styles/App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import Landing from "./pages/Landing";
+import OwnerForm from "./pages/OwnerForm";
+import BusinessProfilePage from "./pages/BusinessProfilePage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        
         {/* route examples: */}
         {/* 
         <Route exact path="/" element={<Bars/>}>
@@ -19,11 +19,20 @@ function App() {
         </Route> */}
         <Route exact path="/" element={<Landing></Landing>} />
         <Route exact path="/login" element={<Login></Login>} />
-        <Route exact path="/ownerCreateForm" element={<OwnerForm></OwnerForm>} />
+        <Route
+          exact
+          path="/ownerCreateForm"
+          element={<OwnerForm></OwnerForm>}
+        />
+        <Route
+          exact
+          path="/business-profile"
+          element={<BusinessProfilePage />}
+        />
         <Route path="*" element={<NotFound></NotFound>} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/appointment-scheduler/src/pages/BusinessProfilePage.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import Layout from "../components/LayOut";
+import Debug from "../components/Debug";
+import '../styles/BusinessProfilePage.css'
+
+function BusinessProfilePage() {
+    return (
+        <Layout>
+            <Debug text="اینجام"></Debug>
+        </Layout>
+    )
+}
+
+export default BusinessProfilePage;
